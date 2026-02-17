b0d13ba7b63438b766e71f17dc2977f24ae4432e
Pricetagjmd <pricejordayne@gmail.com>
2026-02-17 12:53:50 -0500

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,6 @@
+import { env } from "@mealcraft/env/server";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+import * as schema from "./schema";
+
+export const db = drizzle(env.DATABASE_URL, { schema });
